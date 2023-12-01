02a31e43688c689deaaa3b5799d94a3966a26e4e
Sampo Siltanen <sampo.siltanen@gmail.com>
2023-12-01 18:07:57 +0200

    Day 1 remastered

--- fsharp/day01.fsx
+++ fsharp/day01.fsx
@@ -1,37 +1,31 @@
-open System
-open System.Text.RegularExpressions
+let data = System.IO.File.ReadAllLines "data/day01.txt"
 
-let toInt (c: char) = int c - int '0'
+#time
 
-let data = IO.File.ReadAllLines "data/day01.txt"
+let inline toInt (c: char) = int c - int '0'
 
-#time "on"
-
-data
-|> Array.sumBy (fun chars ->
-    let first = chars |> Seq.find Char.IsNumber |> toInt
-    let last = chars |> Seq.findBack Char.IsNumber |> toInt
-    10 * first + last)
-|> printfn "Part 1: %i"
+let part1 str =
+    let first = Seq.find System.Char.IsAsciiDigit >> toInt
+    let last = Seq.findBack System.Char.IsAsciiDigit >> toInt
+    10 * (first str) + (last str)
 
 let numbers =
     [| "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine" |]
 
-data
-|> Array.sumBy (fun line ->
-    let numbers =
-        seq line
+let part2 (str: string) =
+    let matches =
+        str
         |> Seq.indexed
         |> Seq.choose (function
-            | i, c when Char.IsNumber(c) -> Some(toInt c)
-            | i, c ->
-                let substr = line.Substring(i)
+            | _, c when System.Char.IsAsciiDigit c -> Some(toInt c)
+            | i, _ ->
+                let substr = str.Substring(i)
 
-                numbers
-                |> Array.tryFindIndex (fun num -> substr.StartsWith(num))
-                |> Option.map ((+) 1))
+                numbers |> Array.tryFindIndex substr.StartsWith |> Option.map ((+) 1))
 
-    10 * (Seq.head numbers) + (Seq.last numbers))
-|> printfn "Part 2: %i"
+    10 * (Seq.head matches) + (Seq.last matches)
 
-#time "off"
+data |> Array.sumBy part1 |> printfn "Part 1: %i"
+data |> Array.sumBy part2 |> printfn "Part 2: %i"
+
+#time
