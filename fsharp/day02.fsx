f16a06f73ed84fa3a6b56519d275175f5b9dac4c
Sampo Siltanen <sampo.siltanen@gmail.com>
2023-12-02 13:43:27 +0200

    Day 2 remastered

--- fsharp/day02.fsx
+++ fsharp/day02.fsx
@@ -1,44 +1,32 @@
 #time
 
+open System
+
 let parseGame (game: string) =
-    let [| id; subsets |] = game[5..].Replace(" ", "").Split(":")
-    int id, subsets.Split ";"
+    let id :: sets =
+        game[5..].Split([| ": "; ", "; "; " |], StringSplitOptions.RemoveEmptyEntries)
+        |> Array.toList
 
-let games = System.IO.File.ReadAllLines "data/day02.txt" |> Array.map parseGame
+    int id, sets |> List.toArray |> Array.map _.Replace(" ", "")
 
-let takeCubes = Seq.takeWhile System.Char.IsDigit >> System.String.Concat >> int
-let takeColor = Seq.skipWhile System.Char.IsDigit >> System.String.Concat
+let games = IO.File.ReadAllLines "data/day02.txt" |> Array.map parseGame
 
-let limits = [| "red", 12; "green", 13; "blue", 14 |]
+let count = Seq.takeWhile Char.IsDigit >> String.Concat >> int
+let color = Seq.skipWhile Char.IsDigit >> String.Concat
+
+let limits = dict [| "red", 12; "green", 13; "blue", 14 |]
 
 let part1 (id: int, game: string[]) =
     game
-    |> Array.forall (fun subset ->
-        subset.Split ","
-        |> Array.forall (fun cubeColor ->
-            let color = takeColor cubeColor
-            let cubes = takeCubes cubeColor
-
-            limits |> Array.find (fst >> (=) color) |> snd |> (<=) cubes))
+    |> Array.forall (fun cubes -> count cubes <= limits.Item(color cubes))
     |> fun isPossible -> if isPossible then id else 0
 
-let part2: int * string[] -> int =
-    snd
-    >> Array.collect (fun subset ->
-        let colors = subset.Split(",")
-
-        let tryGetCubes color =
-            colors
-            |> Array.tryFind (takeColor >> (=) color)
-            |> Option.map (fun cubes -> color, takeCubes cubes)
-
-        [| tryGetCubes "red"; tryGetCubes "green"; tryGetCubes "blue" |]
-        |> Array.choose id)
-    >> Array.groupBy fst
-    >> Array.map (snd >> Array.map snd >> Array.max)
+let part2 =
+    Array.groupBy color
+    >> Array.map (snd >> Array.map count >> Array.max)
     >> Array.reduce (*)
 
 games |> Array.sumBy part1 |> printfn "Part 1: %i"
-games |> Array.sumBy part2 |> printfn "Part 2: %i"
+games |> Array.sumBy (snd >> part2) |> printfn "Part 2: %i"
 
 #time
