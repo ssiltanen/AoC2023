2bea7d70fbd8e33b0c1d90e8ea759c767e1cf381
Sampo Siltanen <sampo.siltanen@gmail.com>
2023-12-03 15:41:03 +0200

    Day 3 works but is ugly

--- fsharp/day03.fsx
+++ fsharp/day03.fsx
@@ -1,4 +1,7 @@
 let data = System.IO.File.ReadAllLines "data/day03.txt"
+
+#time
+
 let height = Array.length data
 let width = data[0].Length
 
@@ -9,6 +12,12 @@ let adjacentToSymbol y x length =
     |> List.exists (fun (x, y) ->
         let c = data[y][x]
         c <> '.' && System.Char.IsDigit c |> not)
+
+let adjacent (x, y) =
+    ([ x - 1 .. x + 1 ], [ y - 1 .. y + 1 ])
+    ||> List.allPairs
+    |> List.where ((<>) (x, y))
+    |> set
 
 let rec part1 (acc: int) (cursor: int) (y: int) (row: string) =
     if row = "" then
@@ -23,4 +32,44 @@ let rec part1 (acc: int) (cursor: int) (y: int) (row: string) =
 
         part1 acc (cursor + nonNum.Length) y row[nonNum.Length ..]
 
+let rec parse (acc: ((int * int) Set * int) list) (cursor: int) (y: int) (row: string) =
+    if row = "" then
+        acc
+    elif System.Char.IsDigit row[0] then
+        let num = row |> Seq.takeWhile System.Char.IsDigit |> System.String.Concat
+
+        let coords =
+            [ cursor .. cursor + num.Length - 1 ] |> List.map (fun x -> x, y) |> set
+
+        parse ((coords, int num) :: acc) (cursor + num.Length) y row[num.Length ..]
+
+    else
+        let nonNum =
+            row |> Seq.takeWhile (System.Char.IsDigit >> not) |> System.String.Concat
+
+        parse acc (cursor + nonNum.Length) y row[nonNum.Length ..]
+
+let numbers = data |> Array.mapi (parse [] 0)
+
+let part2 =
+    data
+    |> Array.mapi (fun y row -> row |> Seq.indexed |> Seq.where (snd >> (=) '*') |> Seq.map (fun (x, _) -> x, y))
+    |> Array.collect Seq.toArray
+    |> Array.sumBy (fun (astX, astY) ->
+        let adj = adjacent (astX, astY)
+
+        numbers
+        |> Array.map (
+            List.choose (fun (c, num) ->
+                if Set.intersect adj c |> Set.isEmpty |> not then
+                    Some num
+                else
+                    None)
+        )
+        |> Array.collect List.toArray
+        |> fun arr -> if Array.length arr = 2 then Array.reduce (*) arr else 0)
+
 data |> Array.mapi (part1 0 0) |> Array.sum |> printfn "Part 1: %i"
+part2 |> printfn "Part 2: %i"
+
+#time
